5efe7563b94e1ffe7f57d1a8a87bce71fbcc2bbe
watsonian <watsonian@gmail.com>
2010-02-08 22:53:24 -0800

    More style changes. jQuery niceness. Support for creating/cleaning logs.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,36 @@
 $(document).ready(function(){
-  $('ul').click(function(){
-    $(this).find('li.procs').slideToggle();
+  $('pre').click(function(){
+    return false;
+  });
+  
+  $('.processes ul').click(function(){
+    $('.processes ul li.procs').slideUp();
+    if ($(this).find('li.procs').is(':hidden')) {
+      $(this).find('li.procs').slideDown();
+    }
+  });
+  
+  $('#max_memused_form').submit(function(){
+    $.ajax({
+      type: 'PUT',
+      data: 'memory_threshold='+ $("input#memory_threshold").val(),
+      url: '/max_memused',
+      success: function(data){
+        $('#memory_threshold_result').addClass('success').html(data);
+      },
+      error: function(data){
+        $('#memory_threshold_result').addClass('failed').html(data);
+      },
+      complete: function(){
+        $('#memory_threshold_result').fadeOut(3000, function(){
+          $(this).html('').removeClass('success failed').show();
+        });
+      }
+    });
+    return false;
+  });
+  
+  $('#max_memused_form input').click(function(){
+    $(this).select();
   });
 });

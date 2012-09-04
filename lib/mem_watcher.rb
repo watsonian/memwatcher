49fb667db19f7e0f22d79ae5db57e013788cd526
watsonian <watsonian@gmail.com>
2012-09-03 21:21:17 -0700

    Better ps command to capture running procs.

--- lib/mem_watcher.rb
+++ lib/mem_watcher.rb
@@ -73,7 +73,7 @@ class MemWatcher < Sinatra::Base
 
     def save_process_list!(cur_meminfo)
       FileUtils.mkdir_p("./log/procs")
-      system("ps auxr > log/procs/`date \"+%Y%m%d_%H%M%S-#{@@max_memused}-#{cur_meminfo.memused}\"`.snapshot.out")
+      system("ps auxf > log/procs/`date \"+%Y%m%d_%H%M%S-#{@@max_memused}-#{cur_meminfo.memused}\"`.snapshot.out")
     end
 
     def check_cleanup!
